883f71bb8f2d530363cb3e47ae5e7bcb29a89373
ndalilavin <vinson.steve@student.moringaschool.com>
2022-06-17 20:46:37 +0300

    commit tree a3e7bc3f95cd463f9f69d6dc6a02d0acfaaeb104

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const len = word.length;
+
+  for (let i = 0; i<len/2; i++) {
+    if (word[i] !== word[len - 1 - i]) {
+      return false;
+    }
+    return true;
+  }
 }
 
 /* 
   Add your pseudocode here
+  - Reverse the word to check 
+  - Check if the reversed string is the same as the initial string 
 */
 
 /*
   Add written explanation of your solution here
+  The function checks whether the first character in the word provided matches its last character.
+  It then return true if they match or false if they it doesnt.
+
 */
 
 // You can run `node index.js` to view these console logs
